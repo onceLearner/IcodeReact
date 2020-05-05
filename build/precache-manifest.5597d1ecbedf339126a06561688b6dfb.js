e211e85c8225b0f3ba089615c4ba715ce3d68c91
OnceLearner <redaamir972@gmail.com>
2020-05-05 11:48:18 +0000

    test travisdkdf

---
+++ build/precache-manifest.5597d1ecbedf339126a06561688b6dfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec6f15a5e3af15eb091eb728e67df80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a78773fc4da4f6f4212",
+    "url": "/static/css/main.22378bf6.chunk.css"
+  },
+  {
+    "revision": "0a7fb257ff428b8e83fb",
+    "url": "/static/js/2.375b3c7f.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.375b3c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a78773fc4da4f6f4212",
+    "url": "/static/js/main.719e12c6.chunk.js"
+  },
+  {
+    "revision": "48f43ed522c2cb86a313",
+    "url": "/static/js/runtime-main.3fe8e286.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

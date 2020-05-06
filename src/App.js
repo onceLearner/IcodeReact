2dba9c5c78b64e567fea5dc7cfdc9f744d8fc80e
OnceLearner <redaamir972@gmail.com>
2020-05-06 04:22:10 +0000

    change travis with deployddd

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import Signup from "./Components/signup";
 import Login from "./Components/login";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Router } from "react-router-dom";
 import LoginSingup from "./loginSignup";
 import Interface from "./interface";
 import Landing from "./landing";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Landing />
-      </Route>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
 
-      <Route path="/auth">
-        <LoginSingup />
-      </Route>
-      <Route exact path="/user">
-        <Interface />
-      </Route>
-    </Switch>
+        <Route path="/auth">
+          <LoginSingup />
+        </Route>
+        <Route exact path="/user">
+          <Interface />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 export default App;

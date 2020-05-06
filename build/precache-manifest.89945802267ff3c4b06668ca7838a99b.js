0fe32a73dbbfa2c58c19b8b6b692d9e0b57b77c4
OnceLearner <redaamir972@gmail.com>
2020-05-06 02:34:18 +0000

    actually routes work on firebase

---
+++ build/precache-manifest.89945802267ff3c4b06668ca7838a99b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2e47e993742df795e979e5a5bf9a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7780eb1a9e8e44e6ef24",
+    "url": "/static/css/main.22378bf6.chunk.css"
+  },
+  {
+    "revision": "0a7fb257ff428b8e83fb",
+    "url": "/static/js/2.375b3c7f.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.375b3c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7780eb1a9e8e44e6ef24",
+    "url": "/static/js/main.048a887c.chunk.js"
+  },
+  {
+    "revision": "48f43ed522c2cb86a313",
+    "url": "/static/js/runtime-main.3fe8e286.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
